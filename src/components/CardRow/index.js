34448a80da8f7adf8e4516bc6da8ce2ee33a680c
Niip07 <lucasburgardt07@gmail.com>
2022-01-23 10:26:35 -0300

    cards dinamicos

--- src/components/CardRow/index.js
+++ src/components/CardRow/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import Card from "./Card";
+import api from "../../services/api";
 import { CardRowContainer } from "./styles";
 
 export default function CardRow() {
+    const [cards, setCards] = useState([])
+    const getCards = useCallback(async() => {
+        try {
+            const response = await api.get("/points");
+            setCards(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    },[])
+
+    useEffect(() => {
+        getCards();
+    },[])
+
     return (
         <CardRowContainer>
-            <Card title="Pontuação esperada" info="1500"/>
-            <Card title="Pontuação média" info="1500"/>
-            <Card title="Porcentagem média" info="80%"/>
+            <Card title="Pontuação esperada" info={cards[0]}/>
+            <Card title="Pontuação total" info={cards[1]}/>
+            <Card title="Porcentagem alcançada" info={`${cards[2]}%`}/>
         </CardRowContainer>
     )
 }
